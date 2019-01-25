c01cae4a7a84e2f7c4e4d3ccb7eea0acab920485
jakubkarczewski <jakub.karczewski@gmail.com>
2019-01-25 17:57:09 +0100

    Add polish translations for admin panel and more

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,13 @@ from django.utils import timezone
 
 class Post(models.Model):
 
-    author = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    author = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, verbose_name='autor')
+    title = models.CharField(max_length=200, verbose_name='tytuł')
+    text = models.TextField(verbose_name='tekst')
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=timezone.now, verbose_name='data utworzenia')
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            blank=True, null=True, verbose_name='data_opublikowania')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -19,13 +19,17 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posty'
+
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
-    author = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    approved_comment = models.BooleanField(default=False)
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments', verbose_name='post')
+    author = models.CharField(max_length=200, verbose_name='autor')
+    text = models.TextField(verbose_name='tekst')
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='data utworzenia')
+    approved_comment = models.BooleanField(default=False, verbose_name='data_opublikowania')
 
     def approve(self):
         self.approved_comment = True
@@ -37,3 +41,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+    class Meta:
+        verbose_name = 'Komentarz'
+        verbose_name_plural = 'Komentarze'
